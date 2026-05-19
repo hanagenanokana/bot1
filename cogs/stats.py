958154e1bd5c4a8c66fd23a8c98697975cecd611
hanagenanokana <hajime20061212@gmail.com>
2026-05-19 21:51:06 +0900

    Update stats.py

--- cogs/stats.py
+++ cogs/stats.py
@@ -2,12 +2,12 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 
-class Ping(commands.Cog):
+class stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="stats", description="stats")
-    async def ping(self, interaction: discord.Interaction):
+    async def stats(self, interaction: discord.Interaction):
         await interaction.response.send_message("雑魚！wwwwwwwwwww")
 
 async def setup(bot):
