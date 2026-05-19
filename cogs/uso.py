d480c8bf181eddfd07ead676e218511f208f781d
hanagenanokana <hajime20061212@gmail.com>
2026-05-19 21:51:31 +0900

    Update uso.py

--- cogs/uso.py
+++ cogs/uso.py
@@ -2,12 +2,12 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 
-class Hello(commands.Cog):
+class uso(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="uso", description="餅")
-    async def hello(self, interaction: discord.Interaction):
+    async def uso(self, interaction: discord.Interaction):
         await interaction.response.send_message("噓つきは餅の始まり")
 
 async def setup(bot):
