d30eb03ede8e3cd9d1b11dbd7df9a224f8c737b2
hanagenanokana <hajime20061212@gmail.com>
2026-05-21 16:10:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ threading.Thread(target=run_web).start()
 
 # ===== Discord Bot =====
 intents = discord.Intents.default()
+
+# ロールメンバー取得用
 intents.members = True
+
+# !c 20 みたいな通常コマンド用
 intents.message_content = True
 
 bot = commands.Bot(
@@ -32,26 +36,43 @@ bot = commands.Bot(
 
 # ===== Cog Load =====
 async def load():
+
+    # 参加募集
     await bot.load_extension("cogs.rankrole")
+
+    # MMR関連
     await bot.load_extension("cogs.player")
+
+    # サイコロ
     await bot.load_extension("cogs.dice")
+
+    # stats
     await bot.load_extension("cogs.stats")
+
+    # uso
     await bot.load_extension("cogs.uso")
 
+# ===== Ready =====
 @bot.event
 async def on_ready():
+
     print(f"ログイン成功: {bot.user}")
 
-async def main():
-    async with bot:
-        await load()
+    try:
+        synced = await bot.tree.sync()
+        print(f"{len(synced)}個のコマンドを同期しました")
 
-        try:
-            synced = await bot.tree.sync()
-            print(f"{len(synced)}個のコマンドを同期しました")
-        except Exception as e:
-            print(e)
+    except Exception as e:
+        print(e)
+
+# ===== Main =====
+async def main():
+
+    async with bot:
+
+        await load()
 
         await bot.start(TOKEN)
 
+# ===== Run =====
 asyncio.run(main())
