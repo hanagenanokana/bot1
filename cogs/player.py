d757ea0db3949db3eef2b29a73433d8811d91cb3
hanagenanokana <hajime20061212@gmail.com>
2026-05-28 16:08:49 +0900

    Update player.py

--- cogs/player.py
+++ cogs/player.py
@@ -7,7 +7,7 @@ import statistics
 from services.lounge_api import fetch_mmr
 from services.lounge_api import fetch_peak
 
-class Player(commands.groupCog):
+class Player(commands.GroupCog):
 
     def __init__(self, bot):
         self.bot = bot
